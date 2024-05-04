0fe410615c1ee46037c5ccaf065ff079cc06c1e6
Spenser Andreassen <spenserca@gmail.com>
2024-05-04 11:27:09 -0500

    clean up old todo references

--- Program.cs
+++ Program.cs
@@ -5,21 +5,13 @@ var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    // options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
 builder.Services.AddGraphQLServer()
     .AddQueryType<Query>();
 
 var app = builder.Build();
-
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
 
 app.MapGraphQL();
 app.UseRouting();
